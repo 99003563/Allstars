28f3e14b55abcc5ff08efa77e8f53f224d66a455
Saloni-Adanna <69193723+Saloni-Adanna@users.noreply.github.com>
2021-02-05 12:15:54 +0530

    Saloni
    MULT_DIV

---
+++ Implementation/inc/multiplication.h
@@ -0,0 +1,18 @@
+/**
+ * @file Multiplication.h
+ * @author Saloni
+ * @brief Multiplication.h file for mathamatical operations
+ * @version 0.1
+ * @date 2021-02-05
+ * 
+ */
+
+#ifndef __MULTIPLICATION_H__
+#define __MULTIPLICATION_H__
+
+#include <stdio.h>
+
+int multiplication();
+
+#endif
+
